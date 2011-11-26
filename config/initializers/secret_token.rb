85ca4cdddc066572a158cf37231bc05dab9f8053
Daniel Becker <daniel.becker@me.com>
2011-11-26 14:06:52 +0100

    Created initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DumboEasyCreate::Application.config.secret_token = '77253b6a4ee78f06641a99a95756707b87fba3beae551e97ab6f46151790a2b1394f4f7eaa559091d14b6fe863cbfa021ed92617c3471ff8f8955892f008929b'
