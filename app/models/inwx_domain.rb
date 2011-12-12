a29a7e8e0d739c66c82c030c7cfb9a721fe74fc0
Daniel Becker <daniel.becker@me.com>
2011-12-12 18:34:54 +0100

    added dumbo activation status

--- app/models/inwx_domain.rb
+++ app/models/inwx_domain.rb
@@ -4,4 +4,11 @@ class InwxDomain < ActiveRecord::Base
   has_many  :a_records, :dependent => :destroy
   has_many  :cname_records, :dependent => :destroy
   
+  def dumbo_state
+    if self.a_records.where(:entry => "184.106.177.132").exists? && self.cname_records.where(:entry => self.domain, :name => "www")
+      "Dumbo activated"
+    else
+      "Dumbo not activated"
+    end
+  end
 end
