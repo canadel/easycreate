a9f4b9fcf1abb3d5fc4293643dd1529a387099bf
Daniel Becker <daniel.becker@me.com>
2012-01-09 00:24:27 +0100

    deploy.rb typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "easycrate"
+set :application, "easycreate"
 set :repository,  "git@dev.time-is-money.org:dumbo_easy_create.git"
 
 set :scm, :git
@@ -15,8 +15,8 @@ set :sudo_prompt, ""
 
 
 
-role :app, "easycrate.time-is-money.org"                          # This may be the same as your `Web` server
-role :db,  "easycrate.time-is-money.org", :primary => true # This is where Rails migrations will run
+role :app, "easycreate.time-is-money.org"                          # This may be the same as your `Web` server
+role :db,  "easycreate.time-is-money.org", :primary => true # This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
