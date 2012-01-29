2deb544e6e23185d1745f7e36fe62ac2a739e938
Yury Batenko <jurbat@gmail.com>
2012-01-29 18:22:55 +0800

    Implement Easy create template choose page

--- app/controllers/inwx_domains_controller.rb
+++ app/controllers/inwx_domains_controller.rb
@@ -8,7 +8,6 @@ class InwxDomainsController < ApplicationController
   def activate_dumbo
     
     # begin
-      domrobot.login( current_user.inwx_credential.username, current_user.inwx_credential.password)
       if current_user.inwx_domains.find(resource).a_records.where(:entry => "www.#{resource.domain}").exists?
         unless current_user.inwx_domains.find(resource).a_records.where(:name => "www.#{resource.domain}").first.eql?(ENV['DUMBO_IP'])
           record_id = resource.a_records.where(:name => "#{resource.domain}").first.inwx_id
@@ -24,27 +23,23 @@ class InwxDomainsController < ApplicationController
       end
  
       if current_user.inwx_domains.find(resource).cname_records.where(:name => "www.#{resource.domain}").exists?
-        unless current_user.inwx_domains.find(domain).cname_records.where(:name => "www.#{domain.domain}", :entry => domain).exists?
-          record_id = domain.cname_records.where(:name => "www.#{domain.domain}").first.inwx_id
+        unless current_user.inwx_domains.find(domain).cname_records.where(:name => "www.#{resource.domain}", :entry => resource).exists?
+          record_id = resource.cname_records.where(:name => "www.#{resource.domain}").first.inwx_id
           domrobot.call(  'nameserver',
                           'updateRecord',
                           { :id => record_id,
-                            :content => domain.domain
+                            :content => resource.domain
                           }
                        )
         end
       else
-        domrobot.call('nameserver','createRecord', {:domain => domain.domain, :type => 'CNAME', :content => "#{domain.domain}", :name => 'www'})
+        domrobot.call('nameserver','createRecord', {:domain => resource.domain, :type => 'CNAME', :content => "#{resource.domain}", :name => 'www'})
       end
-      redirect_to :action => update_domain, :id => domain.id, :update_view => true
+      redirect_to :action => update_domain, :id => resource.id, :update_view => true
   end
   
   def deactivate_dumbo
-    domrobot = INWX::Domrobot.new(ENV['INWX_DOMROBOT'])
-    domain = InwxDomain.find(params[:domain_id])
-    
     begin
-      domrobot.login( current_user.inwx_credential.username, current_user.inwx_credential.password)
       domrobot.call(  'nameserver',
                       'deleteRecord',
                       { :id => domain.a_records.where(:name => domain.domain).first.inwx_id }
@@ -57,7 +52,7 @@ class InwxDomainsController < ApplicationController
       
     end
     
-    redirect_to :action => update_domain, :id => domain.id, :update_view => true
+    redirect_to :action => update_domain, :id => resource.id, :update_view => true
   end
   
   def update_domains
@@ -73,29 +68,25 @@ class InwxDomainsController < ApplicationController
   
   
   def update_domain(domain_id = params[:domain_id], update_view = params[:update_view])
-    domrobot = INWX::Domrobot.new(ENV['INWX_DOMROBOT'])
-    domrobot.login( current_user.inwx_credential.username, current_user.inwx_credential.password)
     
-    domain = InwxDomain.find(domain_id)
-    
-    @extracted_a_records = Array.new
-    @extracted_cname_records = Array.new
-    @a_records = domrobot.call('nameserver','info', {:domain => domain.domain, :type => 'A'})
-    @cname_records = domrobot.call('nameserver','info', {:domain => domain.domain, :type => 'CNAME'})
+    @extracted_a_records = []
+    @extracted_cname_records = []
+    @a_records = domrobot.call('nameserver','info', {:domain => resource.domain, :type => 'A'})
+    @cname_records = domrobot.call('nameserver','info', {:domain => resource.domain, :type => 'CNAME'})
     #  extracting A Records
     unless @a_records['resData'].blank?
       @a_records['resData']['record'].each do |r|
         @extracted_a_records << ARecord.new(:entry => r['content'], :name => r['name'], :inwx_id => r['id']) unless r.blank?
       end
     end
-    domain.a_records = @extracted_a_records
+    resource.a_records = @extracted_a_records
     #  extracting CNAME Records
     unless @cname_records['resData'].blank?
       @cname_records['resData']['record'].each do |r|
         @extracted_cname_records << CnameRecord.new(:entry => r['content'], :name => r['name'], :inwx_id => r['id']) unless r.blank?
       end
     end
-    domain.cname_records = @extracted_cname_records
+    resource.cname_records = @extracted_cname_records
 
     unless update_view.blank?
         respond_to do |format|
@@ -118,8 +109,6 @@ class InwxDomainsController < ApplicationController
 
       format.js do
         
-    
-        domrobot = INWX::Domrobot.new(ENV['INWX_DOMROBOT'])
     
         begin
           domrobot.login( current_user.inwx_credential.username, current_user.inwx_credential.password)
@@ -147,7 +136,11 @@ class InwxDomainsController < ApplicationController
   private
   # domain api
   def domrobot
-    @_domrobot ||= INWX::Domrobot.new(ENV['INWX_DOMROBOT'])
+    @_domrobot ||= 
+                  INWX::Domrobot.new(ENV['INWX_DOMROBOT']).tap do |robot|
+                    robot.login( current_user.inwx_credential.username, 
+                                 current_user.inwx_credential.password )
+                  end
   end
 
   # domain
--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,5 +1,9 @@
+require 'ostruct'
+
 class LandingPageController < ApplicationController
+  
   def index
+    @resource=OpenStruct.new
   end
 
 end
