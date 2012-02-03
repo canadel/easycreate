b703d668d58a95ede34f517b7e1dcaa2cd3438df
Yury Batenko <jurbat@gmail.com>
2012-02-03 15:54:30 +0800

    Refactore domain and page specs to use segs

--- spec/apis/dumbo/domain_spec.rb
+++ spec/apis/dumbo/domain_spec.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 require 'norails_spec_helper'
 require File.join(APP_ROOT, "lib/external/dumbo/dumbo.rb")
+require File.expand_path(File.join(File.dirname(__FILE__), 'crud_seg'))
 
 require 'pp'
 
@@ -18,202 +19,24 @@ describe Dumbo::Domain do
       'wildcard' => nil }
   end
 
-  let(:non_exist_domain_id){ 999999 }
-  let(:exist_domain_id    ){ 1      }
-
-  def stub_valid_request_on_domain_non_exists(method = :get)
-    stub_request(method, "http://www.dumbocms.com/api/v1/domains/#{non_exist_domain_id}.json").
-           with(:headers => {'X-Auth-Key'=>'7d74e4f46d6459e4ad7b78beb560c718'}).
-           to_return(:status => 404, :body => "", :headers => {})
+  let(:records) do
+   [
+    {'name'     => 'pro7deals.de', 
+     'page_id'  => 1, 
+     'wildcard' => nil }
+    ] 
   end
 
-  def stub_valid_request_on_exists_domain(method = :get)
-    stub_request(method, "http://www.dumbocms.com/api/v1/domains/#{exist_domain_id}.json").
-         with(:headers => {'X-Auth-Key'=>'7d74e4f46d6459e4ad7b78beb560c718'}).
-         to_return(:status => 200, 
-                   :body => json_encode(record),
-                   :headers => {"Content-Type"=>"application/json; charset=utf-8"})
+  let(:non_exist_resource_id){ 999999 }
+  let(:exist_resource_id    ){ 1      }
+
+  let(:resource) { 'domains' }
+  let(:resources){ 'domains' }
+
+
+  %w( index show create update delete ).each do |action|
+    it_should_behave_like action
   end
-
-  def stub_valid_request_index_on_empty(method = :get)
-    stub_request(method, "http://www.dumbocms.com/api/v1/domains.json").
-           with(:headers => {'X-Auth-Key'=>'7d74e4f46d6459e4ad7b78beb560c718'}).
-           to_return(:status => 200, :body => "", :headers => {})
-  end
-
-  def stub_valid_request_index_having_records(method = :get)
-    stub_request(method, "http://www.dumbocms.com/api/v1/domains.json").
-         with(:headers => {'X-Auth-Key'=>'7d74e4f46d6459e4ad7b78beb560c718'}).
-         to_return(:status => 200, 
-                   :body => json_encode(records),
-                   :headers => {"Content-Type"=>"application/json; charset=utf-8"})
-  end
-
-  context 'when GET index' do
-
-    let(:records) do
-     [
-      {'name'     => 'pro7deals.de', 
-       'page_id'  => 1, 
-       'wildcard' => nil }
-      ] 
-    end
-
-    specify{ described_class.should respond_to(:index) }
-
-    it 'should make request with valid path' do
-      stub_valid_request_index_on_empty
-      described_class.index
-      WebMock.should have_requested(:get, "http://www.dumbocms.com/api/v1/domains.json")
-    end
-
-    describe 'when domains empty' do
-      it 'should return empty' do
-        stub_valid_request_index_on_empty
-        response = described_class.index
-        response.body.should be_empty
-      end
-    end # when domains empty
-
-    describe 'when pages have records' do
-      it 'should return json array of records' do
-        stub_valid_request_index_having_records
-        response = described_class.index
-        response.body.should_not be_empty
-        response.parsed_response.should == records
-      end
-    end # when domains have records
-
-  end # context 'GET /index' 
-
-
-
-  context 'SHOW particular domain' do
-
-    context 'when domain exists' do
-      it 'should be success' do
-        stub_valid_request_on_exists_domain
-        domain = described_class.new(exist_domain_id)
-        response = domain.show
-        WebMock.should have_requested(:get, "http://www.dumbocms.com/api/v1/domains/#{exist_domain_id}.json")
-        response.code.should == 200
-      end
-
-      it 'should return domain json object by requested id' do
-        stub_valid_request_on_exists_domain
-        domain = described_class.new(exist_domain_id)
-        response = domain.show
-        response.parsed_response.should == record
-      end
-    end # context 'when domain exists'
-
-
-    context 'when domain not exists' do
-      it 'should raise NotFound error' do
-        stub_valid_request_on_domain_non_exists
-        domain = described_class.new(non_exist_domain_id)
-        lambda{
-          domain.show
-        }.should raise_error(StandardError, 'Net::HTTPNotFound')
-      end
-    end # context 'when domain not exists'
-
-  end # context 'when GET particular domain'
-
-
-  context 'CREATE domain' do
-
-    context 'when domain valid' do
-      it 'should return success' do
-        stub_valid_request_index_on_empty(:post)
-        domain = described_class.new
-        response = domain.create(record)
-        WebMock.should have_requested(:post, "http://www.dumbocms.com/api/v1/domains.json")
-        response.code.should == 200
-      end
-    end
-
-    context 'when domain invalid' do
-      it 'should raise ArgumentError error' do
-        stub_valid_request_index_on_empty(:post)
-        domain = described_class.new
-        lambda{
-          domain.create(invalid_record)
-        }.should raise_error(ArgumentError)
-      end      
-    end
-
-  end # context 'when CREATE domain'
-
-
-
-  context 'UPDATE domain' do
-
-    context 'when domain exists' do
-      context 'when domain valid' do
-        it 'should be success' do
-          stub_valid_request_on_exists_domain(:put)
-          domain = described_class.new(exist_domain_id)
-          response = domain.update(record)
-          response.code.should == 200
-        end
-      end
-
-      context 'when domain invalid' do
-        it 'should raise ArgumentError error' do
-          stub_valid_request_on_exists_domain(:post)
-          domain = described_class.new(exist_domain_id)
-          lambda{
-            domain.create(invalid_record)
-          }.should raise_error(ArgumentError)
-        end
-      end
-    end # context 'when domain exists'
-
-    context 'when domain not exists' do
-      it 'should raise NotFound error' do
-        stub_valid_request_on_domain_non_exists(:put)
-        domain = described_class.new(non_exist_domain_id)
-        lambda{
-          domain.update(record)
-        }.should raise_error(StandardError, 'Net::HTTPNotFound')
-      end
-    end
-
-  end # context 'when UPDATE domain'
-
-
-
-  context 'DELETE domain' do
-
-    context 'when domain exists' do
-      it 'should be success' do
-        stub_valid_request_on_exists_domain(:delete)
-        domain = described_class.new(exist_domain_id)
-        response = domain.delete
-        WebMock.should have_requested(:delete, "http://www.dumbocms.com/api/v1/domains/#{exist_domain_id}.json")
-        response.code.should == 200
-      end
-
-      it 'should return domain json object by requested id' do
-        stub_valid_request_on_exists_domain(:delete)
-        domain = described_class.new(exist_domain_id)
-        response = domain.delete
-        response.parsed_response.should == record
-      end
-    end # context 'when domain exists'
-
-    context 'when domain not exists' do
-      it 'should raise NotFound error' do
-        stub_valid_request_on_domain_non_exists(:delete)
-        domain = described_class.new(non_exist_domain_id)
-        lambda{
-          domain.delete
-        }.should raise_error(StandardError, 'Net::HTTPNotFound')
-      end
-    end # context 'when domain not exists'
-
-  end # context 'when DELETE domain'
 
 end # Dumbo::API
 
--- spec/apis/dumbo/page_spec.rb
+++ spec/apis/dumbo/page_spec.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 require 'norails_spec_helper'
 require File.join(APP_ROOT, "lib/external/dumbo/dumbo.rb")
+require File.expand_path(File.join(File.dirname(__FILE__), 'crud_seg'))
 
 require 'pp'
 
@@ -21,204 +22,23 @@ describe Dumbo::Page do
       'description' => nil  }
   end
 
-  let(:non_exist_page_id){ 999999 }
-  let(:exist_page_id    ){ 1      }
-
-  def stub_valid_request_on_page_non_exists(method = :get)
-    stub_request(method, "http://www.dumbocms.com/api/v1/pages/#{non_exist_page_id}.json").
-           with(:headers => {'X-Auth-Key'=>'7d74e4f46d6459e4ad7b78beb560c718'}).
-           to_return(:status => 404, :body => "", :headers => {})
+  let(:records) do
+   [
+    { 'name'        => 'pro7deals.de', 
+      'title'       => nil, 
+      'template_id' => 1, 
+      'indexable'   => false, 
+      'account_id'  => 3, 
+      'description' => nil }
+    ] 
   end
 
-  def stub_valid_request_on_exists_page(method = :get)
-    stub_request(method, "http://www.dumbocms.com/api/v1/pages/#{exist_page_id}.json").
-         with(:headers => {'X-Auth-Key'=>'7d74e4f46d6459e4ad7b78beb560c718'}).
-         to_return(:status => 200, 
-                   :body => json_encode(record),
-                   :headers => {"Content-Type"=>"application/json; charset=utf-8"})
-  end
+  let(:non_exist_resource_id){ 999999 }
+  let(:exist_resource_id    ){ 1      }
 
-  def stub_valid_request_index_on_empty(method = :get)
-    stub_request(method, "http://www.dumbocms.com/api/v1/pages.json").
-           with(:headers => {'X-Auth-Key'=>'7d74e4f46d6459e4ad7b78beb560c718'}).
-           to_return(:status => 200, :body => "", :headers => {})
-  end
+  let(:resource) { 'pages' }
+  let(:resources){ 'pages' }
 
-  def stub_valid_request_index_having_records(method = :get)
-    stub_request(method, "http://www.dumbocms.com/api/v1/pages.json").
-         with(:headers => {'X-Auth-Key'=>'7d74e4f46d6459e4ad7b78beb560c718'}).
-         to_return(:status => 200, 
-                   :body => json_encode(records),
-                   :headers => {"Content-Type"=>"application/json; charset=utf-8"})
-  end
-
-  context 'when GET index' do
-
-    let(:records) do
-     [
-      { 'name'        => 'pro7deals.de', 
-        'title'       => nil, 
-        'template_id' => 1, 
-        'indexable'   => false, 
-        'account_id'  => 3, 
-        'description' => nil }
-      ] 
-    end
-
-    specify{ described_class.should respond_to(:index) }
-
-    it 'should make request with valid path' do
-      stub_valid_request_index_on_empty
-      described_class.index
-      WebMock.should have_requested(:get, "http://www.dumbocms.com/api/v1/pages.json")
-    end
-
-    describe 'when pages empty' do
-      it 'should return empty' do
-        stub_valid_request_index_on_empty
-        response = described_class.index
-        response.body.should be_empty
-      end
-    end # when pages empty
-
-    describe 'when pages have records' do
-      it 'should return json array of records' do
-        stub_valid_request_index_having_records
-        response = described_class.index
-        response.body.should_not be_empty
-        response.parsed_response.should == records
-      end
-    end # when pages have records
-
-  end # context 'GET /index' 
-
-
-
-  context 'SHOW particular page' do
-
-    context 'when page exists' do
-      it 'should be success' do
-        stub_valid_request_on_exists_page
-        page = described_class.new(exist_page_id)
-        response = page.show
-        WebMock.should have_requested(:get, "http://www.dumbocms.com/api/v1/pages/#{exist_page_id}.json")
-        response.code.should == 200
-      end
-
-      it 'should return page json object by requested id' do
-        stub_valid_request_on_exists_page
-        page = described_class.new(exist_page_id)
-        response = page.show
-        response.parsed_response.should == record
-      end
-    end # context 'when page exists'
-
-
-    context 'when page not exists' do
-      it 'should raise NotFound error' do
-        stub_valid_request_on_page_non_exists
-        page = described_class.new(non_exist_page_id)
-        lambda{
-          page.show
-        }.should raise_error(StandardError, 'Net::HTTPNotFound')
-      end
-    end # context 'when page not exists'
-
-  end # context 'when GET particular page'
-
-
-  context 'CREATE page' do
-    context 'when page valid' do
-      it 'should return success' do
-        stub_valid_request_index_on_empty(:post)
-        page = described_class.new
-        response = page.create(record)
-        WebMock.should have_requested(:post, "http://www.dumbocms.com/api/v1/pages.json")
-        response.code.should == 200
-      end
-    end
-
-    context 'when page invalid' do
-      it 'should raise ArgumentError error' do
-        stub_valid_request_index_on_empty(:post)
-        page = described_class.new
-        lambda{
-          page.create(invalid_record)
-        }.should raise_error(ArgumentError)
-      end      
-    end
-
-  end # context 'when CREATE page'
-
-
-
-  context 'UPDATE page' do
-
-    context 'when page exists' do
-      context 'when page valid' do
-        it 'should be success' do
-          stub_valid_request_on_exists_page(:put)
-          page = described_class.new(exist_page_id)
-          response = page.update(record)
-          response.code.should == 200
-        end
-      end
-
-      context 'when page invalid' do
-        it 'should raise ArgumentError error' do
-          stub_valid_request_on_exists_page(:post)
-          page = described_class.new(:exist_page_id)
-          lambda{
-            page.create(invalid_record)
-          }.should raise_error(ArgumentError)
-        end
-      end
-    end # context 'when page exists'
-
-    context 'when page not exists' do
-      it 'should raise NotFound error' do
-        stub_valid_request_on_page_non_exists(:put)
-        page = described_class.new(non_exist_page_id)
-        lambda{
-          page.update(record)
-        }.should raise_error(StandardError, 'Net::HTTPNotFound')
-      end
-    end
-
-  end # context 'when UPDATE page'
-
-
-
-  context 'DELETE page' do
-
-    context 'when page exists' do
-      it 'should be success' do
-        stub_valid_request_on_exists_page(:delete)
-        page = described_class.new(exist_page_id)
-        response = page.delete
-        WebMock.should have_requested(:delete, "http://www.dumbocms.com/api/v1/pages/#{exist_page_id}.json")
-        response.code.should == 200
-      end
-
-      it 'should return page json object by requested id' do
-        stub_valid_request_on_exists_page(:delete)
-        page = described_class.new(exist_page_id)
-        response = page.delete
-        response.parsed_response.should == record
-      end
-    end # context 'when page exists'
-
-    context 'when page not exists' do
-      it 'should raise NotFound error' do
-        stub_valid_request_on_page_non_exists(:delete)
-        page = described_class.new(non_exist_page_id)
-        lambda{
-          page.delete
-        }.should raise_error(StandardError, 'Net::HTTPNotFound')
-      end
-    end # context 'when page not exists'
-
-  end # context 'when DELETE page'
 
 end # Dumbo::API
 
