c64dbba9b3eda0f7a221dbd5711b56c76d400e6c
Denis Lysenko <crashcube@gmail.com>
2013-02-27 00:13:52 +0400

    * Layout changes

---
+++ app/assets/javascripts/inwx_domains.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('.animated').click(function () {
+    $(this).parent().html('<div id="circle"><div id="circle_1" class="circle"></div><div id="circle_2" class="circle"></div><div id="circle_3" class="circle"></div><div class="clearfix"></div>');
+  });
+});
