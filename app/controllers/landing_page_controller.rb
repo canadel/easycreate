5eced0ed4abd55c5d1a6b9acdbe5ff6642940378
Denis Lysenko <crashcube@gmail.com>
2013-03-23 16:51:17 +0400

    * Activate dumbo in one-click launch

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -23,10 +23,47 @@ class LandingPageController < ApplicationController
         flash[:error] = 'Please, select template'
       end
 
+      resource = InwxDomain.where(:domain => params[:resource][:domain]).first
+
+      if resource.nil?
+        flash[:error] = 'Domain not found, please refresh your domains'
+      end
+      
       unless flash[:error].blank?
         render 'index'
       else 
 
+        # create or update address record
+        if current_user.inwx_domains.find(resource).a_records.where(:entry => "www.#{resource.domain}").exists?
+          unless current_user.inwx_domains.find(resource).a_records.where(:name => "www.#{resource.domain}").first.eql?(ENV['DUMBO_IP'])
+            record_id = resource.a_records.where(:name => "#{resource.domain}").first.inwx_id
+            domrobot.call(  'nameserver',
+                            'updateRecord',
+                            { :id => record_id,
+                              :content => ENV['DUMBO_IP']
+                            }
+                         )
+          end
+        else
+          domrobot.call('nameserver','createRecord', {:domain => resource.domain, :type => 'A', :content => ENV['DUMBO_IP'], :name => ''})
+        end
+
+        # create or update cname record
+        if current_user.inwx_domains.find(resource).cname_records.where(:name => "www.#{resource.domain}").exists?
+          unless current_user.inwx_domains.find(resource).cname_records.where(:name => "www.#{resource.domain}", :entry => resource).exists?
+            record_id = resource.cname_records.where(:name => "www.#{resource.domain}").first.inwx_id
+            domrobot.call(  'nameserver',
+                            'updateRecord',
+                            { :id => record_id,
+                              :content => resource.domain
+                            }
+                         )
+          end
+        else
+          domrobot.call('nameserver','createRecord', {:domain => resource.domain, :type => 'CNAME', :content => "#{resource.domain}", :name => 'www'})
+        end
+
+        # create package via API
         page = Dumbo::Page.new :email => current_user.email 
         
         page_resp = page.create({
